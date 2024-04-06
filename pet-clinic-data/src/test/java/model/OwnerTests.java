6cb25689c2b7c39e8111d76eea0994276bd404a3
SehajparkashSingh-BY <Sehajparkashsingh.Pannu@blueyonder.com>
2024-04-06 16:55:20 +0530

    Create Multi-Module for Data Model

---
+++ pet-clinic-data/src/test/java/model/OwnerTests.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+public class OwnerTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
+
+
+
+
---
+++ pet-clinic-data/src/test/java/model/PersonTests.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class PersonTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
