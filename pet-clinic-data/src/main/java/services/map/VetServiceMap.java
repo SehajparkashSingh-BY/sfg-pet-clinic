37e3f8136e3038378de76146b8faafd29cbf8c74
SehajparkashSingh-BY <Sehajparkashsingh.Pannu@blueyonder.com>
2024-04-13 11:40:51 +0530

    Implement Map Based Services. Closes #4.

---
+++ pet-clinic-data/src/main/java/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package services.map;
+
+import model.Vet;
+import services.CrudServices;
+
+import java.util.Set;
+
+public class VetServiceMap extends  AbstractMapService<Vet,Long> implements CrudServices<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
